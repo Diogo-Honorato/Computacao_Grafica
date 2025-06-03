207c9b218003c63ea7b0a0392e2eb67ec3dedd7b
Diogo-Honorato <honoratodiogo097@gmail.com>
2025-06-03 04:45:24 -0300

    Add objetos da casa

--- source/main.cpp
+++ source/main.cpp
@@ -28,21 +28,67 @@ int main()
     ));
 
     glm::mat4 trans_triangle = glm::mat4(1.0f);
-    trans_triangle = glm::translate(trans_triangle,glm::vec3(-0.5f,0.0f,0.0));
+    trans_triangle = glm::translate(trans_triangle,glm::vec3(-0.3f,0.167f,0.0f));
+    trans_triangle = glm::scale(trans_triangle,glm::vec3(0.65f,0.65f,0.0));
     
     matrices.push_back(trans_triangle);
     //////////////////////////////////////////////////////////////////
 
-    //square
+    //square wall left
     Shapes.push_back(std::make_unique<Square>(
         "../shader/shaders_casa/vertex/vertex_square.vs",
         "../shader/basic_shaders/fragment/basic_square.fs"
     ));
 
-    glm::mat4 trans_square = glm::mat4(1.0f);
-    trans_square = glm::translate(trans_square,glm::vec3(-0.5f,-0.5f,0.0));
+    glm::mat4 trans_wall_left = glm::mat4(1.0f);
+    trans_wall_left = glm::translate(trans_wall_left,glm::vec3(-0.3f,-0.5f,0.0f));
+    trans_wall_left = glm::scale(trans_wall_left,glm::vec3(0.65f,0.65f,0.0));
 
-    matrices.push_back(trans_square);
+    matrices.push_back(trans_wall_left);
+
+    //////////////////////////////////////////////////////////////////
+
+    //square wall right
+    Shapes.push_back(std::make_unique<Square>(
+        "../shader/shaders_casa/vertex/vertex_square.vs",
+        "../shader/basic_shaders/fragment/basic_square.fs"
+    ));
+
+    glm::mat4 trans_wall_right = glm::mat4(1.0f);
+    trans_wall_right = glm::translate(trans_wall_right,glm::vec3(0.337f,-0.4f,0.0f));
+    trans_wall_right = glm::scale(trans_wall_right,glm::vec3(0.705f,0.755f,0.0));
+    trans_wall_right = glm::rotate(trans_wall_right,glm::radians(31.0f),glm::vec3(1.0f,0.0f,0.0f));
+    trans_wall_right = glm::rotate(trans_wall_right,glm::radians(31.0f),glm::vec3(0.0f,1.0f,0.0f));
+
+    matrices.push_back(trans_wall_right);
+    //////////////////////////////////////////////////////////////////
+
+    //square roof
+    Shapes.push_back(std::make_unique<Square>(
+        "../shader/shaders_casa/vertex/vertex_square.vs",
+        "../shader/shaders_casa/fragment/fragment_roof.fs"
+    ));
+
+    glm::mat4 trans_roof = glm::mat4(1.0f);
+    trans_roof = glm::translate(trans_roof,glm::vec3(0.337f,0.0f,0.0f));
+    trans_roof = glm::scale(trans_roof,glm::vec3(0.705f,0.755f,0.0));
+    trans_roof = glm::rotate(trans_roof,glm::radians(131.0f),glm::vec3(0.0f,1.0f,0.0f));
+    trans_roof = glm::rotate(trans_roof,glm::radians(131.0f),glm::vec3(1.0f,0.0f,0.0f));
+
+    matrices.push_back(trans_roof);
+    //////////////////////////////////////////////////////////////////
+    
+    //circle
+    Shapes.push_back(std::make_unique<Circle>(
+        "../shader/shaders_casa/vertex/vertex_circle.vs",
+        "../shader/basic_shaders/fragment/basic_circle.fs"
+    ));
+    
+    glm::mat4 trans_circle = glm::mat4(1.0f);
+    trans_circle = glm::translate(trans_circle,glm::vec3(-0.3f,0.045f,0.0));
+    trans_circle = glm::scale(trans_circle,glm::vec3(0.382f,0.382f,0.0));
+
+    matrices.push_back(trans_circle);
     //////////////////////////////////////////////////////////////////
 
 
@@ -52,7 +98,7 @@ int main()
         processInput(window);
 
         // Limpa tela
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         // Desenha Shapes
